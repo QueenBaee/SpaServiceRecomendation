bee7639155aaa837decca130a95bcd4d6b0b952d
queenbaee <rrobby122@gmail.com>
2024-07-23 15:40:12 +0700

    Added migrations file

---
+++ database/migrations/2024_07_23_083833_tb_perawatanwajah.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tb_perawatanwajah', function (Blueprint $table) {
+            $table->id(); // Kolom id untuk primary key
+            $table->string('nama', 50); // Kolom nama dengan VARCHAR(50)
+            $table->string('alamat', 50); // Kolom alamat dengan VARCHAR(50)
+            $table->string('tanggal_konsultasi', 50); // Kolom tanggal konsultasi dengan VARCHAR(50)
+            $table->string('perawatan_rutin', 50); // Kolom perawatan rutin dengan VARCHAR(50) (default 'Tidak')
+            $table->string('perawatan_sebelumnya', 50); // Kolom perawatan terakhir dengan VARCHAR(50) yang bisa null
+            $table->string('saran_perawatan', 50); // Kolom saran perawatan dengan VARCHAR(50) yang bisa null
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tb_perawatanwajah');
+    }
+};
